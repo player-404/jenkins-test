8a5b064efb24ced54e386f550abc8b2d2ec4be5a
lzp <1315363446@qq.com>
2024-06-19 10:43:08 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import TheWelcome from "./components/TheWelcome.vue";
 
         <div class="wrapper">
             <HelloWorld msg="You did it!" />
-            <h1>自动化部署测试</h1>
+            <h1>自动化部署测试去212121</h1>
         </div>
     </header>
 
